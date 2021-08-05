a046d91bbd2cb4ea164b1cf987d4334b82226b41
Fernando Martinez <petato258@gmail.com>
2021-08-05 00:10:51 -0500

    add enviroment vars

---
+++ config/placetopay.php
@@ -0,0 +1,8 @@
+<?php
+
+ return[
+    'login' => env('PLACETOPAY_LOGIN'),
+     'secretkey' => env('PLACETOPAY_SECRETKEY'),
+     'baseurl' => env('PLACETOPAY_BASE_URL'),
+     'baseurl_pse' => env('PLACETOPAY_BASE_PSE_URL'),
+ ];
